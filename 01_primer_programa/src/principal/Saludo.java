94333367cc39b0137882021dc0f125543511e2bf
Edu <eduardobrunner94@gmil.com>
2022-04-05 21:17:24 -0300

    solo un mensaje de bienvenida

---
+++ 01_primer_programa/src/principal/Saludo.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido a java");
+
+	}
+
+}
