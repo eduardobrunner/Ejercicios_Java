95875dd9c2ac954904c7a5d940f79e96a49b5c83
Edu <eduardobrunner94@gmil.com>
2022-04-05 21:32:13 -0300

    distingue cual es el mayor de 2 numeros

---
+++ 03_ejercicio_practico_2/src/NumeroMayor.java
@@ -0,0 +1,18 @@
+
+public class NumeroMayor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6;
+		int b = 10;
+		int res;
+		
+		if (a > b)
+			System.out.println("El mayor es a=" + a);
+		else if(b>a)
+			System.out.println("El mayor es b=" + b);
+		else
+			System.out.println("Los numeros son iguales");
+	}
+
+}
