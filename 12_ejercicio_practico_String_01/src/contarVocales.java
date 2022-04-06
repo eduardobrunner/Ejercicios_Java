b39aaf1a5826bb34df12106bb2262e8146205fee
Edu <eduardobrunner94@gmil.com>
2022-04-05 21:43:41 -0300

    Uso strings, calculo cantidad de vocales de texto ingresado

---
+++ 12_ejercicio_practico_String_01/src/contarVocales.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class contarVocales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cont=0;
+		int totalVocales=0;
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("Ingrese un texto: ");
+		String texto = "Hola";
+		int[] cantVocales = new int[5];
+		System.out.println(texto.indexOf("a"));
+
+		while (cont < texto.length()) {
+			switch(texto.charAt(cont)) { //texto.charAt(i) devuelve el caracter de la posicion i
+			case 'a': cantVocales[0]++; break;
+			case 'e': cantVocales[1]++; break;
+			case 'i': cantVocales[2]++; break;
+			case 'o': cantVocales[3]++; break;
+			case 'u': cantVocales[4]++; break;
+			}
+		cont++;
+		}
+		
+		for(int i=0; i<cantVocales.length; i++) {
+			if(cantVocales[i] != 0) {
+				totalVocales+=cantVocales[i];
+			}
+		}
+
+		System.out.println("Hay un total de: "+totalVocales+" vocales");
+	}
+
+}
