74c051e93a233260b1a573dddfbcefbd100bf680
Edu <eduardobrunner94@gmil.com>
2022-04-05 21:33:21 -0300

    uso de switch

---
+++ 04_ejercicio_practico_3/src/EvaluarNota.java
@@ -0,0 +1,22 @@
+
+public class EvaluarNota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		switch (a) {
+		case 1:
+		case 2:
+		case 3:
+		case 4: System.out.println("suspenso"); break;
+		case 5:
+		case 6: System.out.println("aprobado"); break;
+		case 7: 
+		case 8: System.out.println("notable"); break;
+		case 9:
+		case 10: System.out.println("sobresaliente"); break;
+		default: System.out.println("nota invalida"); break;
+		}
+	}
+
+}
