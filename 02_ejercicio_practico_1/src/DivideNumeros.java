577fec0610f19bfe125b3148934780e39db226eb
Edu <eduardobrunner94@gmil.com>
2022-04-05 21:18:58 -0300

    interesante logica para dividir numeros

---
+++ 02_ejercicio_practico_1/src/DivideNumeros.java
@@ -0,0 +1,12 @@
+public class DivideNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 60;
+		int num2=20;
+		int div;
+		div=(num1>num2)?num1/num2:num2/num1;
+		System.out.println("La división es: "+div);
+	}
+
+}
