31f8670ac03c272917be8152c36ac4c5a9a2bec6
Edu <eduardobrunner94@gmil.com>
2022-04-16 12:30:05 -0300

    Se prueba la novedad de inferencia de tipo

--- 02_ejercicio_practico_1/src/DivideNumeros.java
+++ 02_ejercicio_practico_1/src/DivideNumeros.java
@@ -2,9 +2,9 @@ public class DivideNumeros {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1 = 60;
-		int num2=20;
-		int div;
+		var num1 = 60;
+		var num2=20;
+		var div=0;
 		div=(num1>num2)?num1/num2:num2/num1;
 		System.out.println("La división es: "+div);
 	}
